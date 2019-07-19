6b736b2dea65a8b48b1bfd40ab84257c3f0666e2
56box <pelatrotraining@gmail.com>
2019-07-19 14:04:48 +0530

    My Second checkin after revert

---
+++ projects/myTraining/myFirst/src/com/pelatro/training/MyGradleBuild.java
@@ -0,0 +1,10 @@
+package com.pelatro.training;
+
+
+public class MyGradleBuild {
+	
+	public static void main(String[] args){
+		System.out.println("Welcome to pelatro!");
+		System.out.println("Your first build is completed successfully");
+	}
+}
