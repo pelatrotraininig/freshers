7a9296fc42cb600b7e1c32e621d482055e7b7c0e
rkoabhi <rkoabhijoshi@gmail.com>
2019-07-22 15:40:20 +0530

    Subtract 2 num

---
+++ rko1/src/com/pelatro/rko1/Subtract.java
@@ -0,0 +1,20 @@
+package com.pelatro.rko1;
+
+ class Subtract1 {
+	int a=2, b=3, c;
+    void sub(){
+    c= a-b;
+    System.out.println(c);
+
+}
+}
+
+public class Subtract{
+public static void main(String args[])
+{
+Subtract1 a =new Subtract1();
+a.sub();
+}
+}
+
+
