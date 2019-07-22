5a373d0bbaaf4dd5408e6b19cf353c7c1cf151ea
sushmavb <sushma.vb@pelatro.com>
2019-07-22 15:33:37 +0530

    fruitproj

---
+++ firstproj.java
@@ -0,0 +1,17 @@
+public class firstproj
+{
+	public static void main(String args[])
+	{
+		fruit f= new fruit();
+		f.mango();
+	}
+}
+
+public class fruit
+{
+	public void mango()
+	{
+		System.out.println("i am mango");
+	}
+}
+
